e57ff90d4f00dad6dd87bce53605ab417df43118
SV-97 <volzstefan97@googlemail.com>
2018-04-30 19:02:59 +0200

    Working Version

--- P3_004_PlottingTest/P3_004_PlottingTest/P3_004_PlottingTest.py
+++ P3_004_PlottingTest/P3_004_PlottingTest/P3_004_PlottingTest.py
@@ -3,39 +3,36 @@ import numpy as np
 import scipy as sp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import math
-import cmath
 
-pi = np.pi;
-R = 14.1;
-L = 53.5e-3;
-C = 66e-6;
-f = np.linspace(0,20000,500000);
-OMEGA = 2 * pi * f;
+pi = np.pi
+R = 14.1
+L = 53.5e-3
+C = 66e-6
+f = np.linspace(0,20000,50000)
+OMEGA = 2 * pi * f
 
+@np.vectorize
 def Y(omega):
-    return np.complex(R/(R**2 + (omega*L)**2), (omega*C - ((omega*L)/(R**2 + (omega*L)**2))));
+    return np.complex(R/(R**2.0 + (omega*L)**2.0), (omega*C - ((omega*L)/(R**2.0 + (omega*L)**2.0))))
 
-def Y_re(omega):
-    return R/(R**2 + (omega*L)**2)
+@np.vectorize
+def Z(omega):
+    return np.complex((R/(R**2.0+(omega*L)**2.0))/((R/(R**2.0+(omega*L)**2.0))**2.0+(omega*C - (omega*L)/(R**2.0+(omega*L)**2.0))**2.0) , (omega*C - (omega*L)/(R**2.0+(omega*L)**2.0))/((R/(R**2.0+(omega*L)**2.0))**2.0+(omega*C - (omega*L)/(R**2.0+(omega*L)**2.0))**2.0))
 
-def Y_im(omega):
-    return (omega*C - ((omega*L)/(R**2 + (omega*L)**2)))
+y = Y(OMEGA)
+z = Z(OMEGA)
 
-f2 = np.vectorize(Y);
-y = f2(OMEGA);
-z = np.divide(y,1);
-y = f2(OMEGA);
-#z = 1/y;
+plt.subplot(2,1,1)
+plt.plot(y.real,y.imag, linewidth = 2, label = "Y")
+plt.ylabel("Imaginärteil")
+plt.xlabel("Realteil")
 
-if z.all() == y.all():
-    print("1");
-else:
-    print("0");
-    pass;
-    
-plt.plot(y.real,y.imag, linewidth = 2, linestyle = "dashed", label = "Y");
-plt.plot(z.real,z.imag, linewidth = 2, label = "Z");
-plt.show();
+plt.subplot(2,1,2)
+plt.plot(z.real,z.imag, linewidth = 2, label = "Z")
+plt.ylabel("Imaginärteil")
+plt.xlabel("Realteil")
 
-#plt.plot(Y_re(OMEGA),Y_im(OMEGA), linewidth = 2, linestyle = "dashed", label = "Y");
+plt.subplots_adjust(wspace = 0.3, hspace = 0.3)
+plt.gcf().canvas.set_window_title("Ortskurven C || (RL)")
+plt.legend()
+plt.show()
