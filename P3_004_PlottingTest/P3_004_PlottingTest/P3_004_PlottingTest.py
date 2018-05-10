ac1e9716cedd6ed1e98febbcb8ceb47483c2e3da
SV-97 <volzstefan97@googlemail.com>
2018-05-10 11:47:01 +0200

    Initial Commit

--- P3_004_PlottingTest/P3_004_PlottingTest/P3_004_PlottingTest.py
+++ P3_004_PlottingTest/P3_004_PlottingTest/P3_004_PlottingTest.py
@@ -3,6 +3,9 @@ import numpy as np
 import scipy as sp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import timeit
+
+t = timeit.Timer()
 
 pi = np.pi
 R = 14.1
@@ -20,19 +23,23 @@ def Z(omega):
     return np.complex((R/(R**2.0+(omega*L)**2.0))/((R/(R**2.0+(omega*L)**2.0))**2.0+(omega*C - (omega*L)/(R**2.0+(omega*L)**2.0))**2.0) , (omega*C - (omega*L)/(R**2.0+(omega*L)**2.0))/((R/(R**2.0+(omega*L)**2.0))**2.0+(omega*C - (omega*L)/(R**2.0+(omega*L)**2.0))**2.0))
 
 y = Y(OMEGA)
-z = Z(OMEGA)
+z = np.divide(Y,1)
+
+print(t.timeit())
 
 plt.subplot(2,1,1)
 plt.plot(y.real,y.imag, linewidth = 2, label = "Y")
 plt.ylabel("Imaginärteil")
 plt.xlabel("Realteil")
+plt.legend()
 
 plt.subplot(2,1,2)
 plt.plot(z.real,z.imag, linewidth = 2, label = "Z")
 plt.ylabel("Imaginärteil")
 plt.xlabel("Realteil")
+plt.legend()
 
 plt.subplots_adjust(wspace = 0.3, hspace = 0.3)
 plt.gcf().canvas.set_window_title("Ortskurven C || (RL)")
-plt.legend()
 plt.show()
+print(t.timeit())
---
+++ P3_005_BubbleSort/P3_005_BubbleSort/P3_005_BubbleSort.py
@@ -0,0 +1,16 @@
+lst = [4,5,7,1,3,8,6,4,10,2,-20,10.5]
+print(lst)
+sorted = []
+
+while len(lst) > 1:
+    for i in range(len(lst)-1,0,-1): #go through array in reverse
+        r = lst[i]
+        l = lst[i-1]
+        if r < l:
+            lst[i] = l
+            lst[i-1] = r
+        if i == 1:
+            sorted.append(lst.pop(0))
+sorted.append(lst.pop(0))
+print(sorted)
+print(lst)
