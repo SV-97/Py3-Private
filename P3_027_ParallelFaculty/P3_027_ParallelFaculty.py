54758e15672468c8a492f30d021e3f9f66317b2f
Unknown <volzstefan97@googlemail.com>
2018-10-01 19:23:36 +0200

    Added a denoise interpolation

--- P3_027_ParallelFaculty/P3_027_ParallelFaculty.py
+++ P3_027_ParallelFaculty/P3_027_ParallelFaculty.py
@@ -4,7 +4,8 @@ import re
 from time import time
 
 import matplotlib.pyplot as plt
-
+import numpy as np
+from scipy.interpolate import interp1d
 def str_n(i):
     return str(i)+"\n"
 def _faculty(range_):
@@ -12,6 +13,42 @@ def _faculty(range_):
     for i in range_:
         x *= i
     return x
+
+def distribute(lst, n):
+    """distributes lst as equal as possible to n elements
+    """
+    base_load = len(lst)//n
+    extra_load = len(lst)%n
+    load_per_element = [base_load for i in range(n)]
+    for i in range(len(load_per_element)):
+        if extra_load <= 0: break
+        load_per_element[i] +=  1 if extra_load > 0 else 0
+        extra_load -= 1
+    indexes = [sum(load_per_element[:i+1]) for i in range(n)]
+    indexes.insert(0, None) #added so that slicing can be used
+    indexes.append(None)
+    elements = [lst[indexes[i]:indexes[i+1]] for i in range(n)]
+    return elements
+
+def denoise(values, x=None, resolution=None):
+    """Denoise input via spline interpolation
+    Args:
+        values  (iterable): Iterable with values that are to be interpolated
+        x      (iteratble): corresponding x values - Defaults to a numpy array from 0 on with 1 for every value
+        resolution   (int): Sets how many segments your original data will be split into for interpolation, lower is closer to original data
+    Returns:
+        tuple of iterables: (x_values_of_interpolation, interpolated_y_values)
+    """
+    resolution = len(values)//10 if resolution is None else resolution
+    segments = len(values)//resolution
+    segments = distribute(values, segments)
+    average = []
+    for segment in segments:
+        average.append(sum(segment)/len(segment))
+    x = np.linspace(0, len(values), len(average)) if x is None else x
+    spline = interp1d(x, average, kind="cubic")
+    y = spline(x)
+    return x, y
 
 def faculty(n, number_of_processes=4):
     def _faculty(range_):
@@ -28,21 +65,6 @@ def faculty(n, number_of_processes=4):
             x = _faculty(self.range)
             self.queue.put(x)
             self.queue.close()
-    def distribute(lst, n):
-        """distributes lst as equal as possible to n elements
-        """
-        base_load = len(lst)//n
-        extra_load = len(lst)%n
-        load_per_element = [base_load for i in range(n)]
-        for i in range(len(load_per_element)):
-            if extra_load <= 0: break
-            load_per_element[i] +=  1 if extra_load > 0 else 0
-            extra_load -= 1
-        indexes = [sum(load_per_element[:i+1]) for i in range(n)]
-        indexes.insert(0, None) #added so that slicing can be used
-        indexes.append(None)
-        elements = [lst[indexes[i]:indexes[i+1]] for i in range(n)]
-        return elements
         
     ranges = distribute(range(1,n+1), number_of_processes)
 
@@ -83,16 +105,23 @@ if __name__ == "__main__":
     else:
         with open(path+"/times2", "r") as f:
             content = f.readlines()
-            T2 = map(float, content)
+            T2 = list(map(float, content))
         for i in procs:
             with open(path+"/times1"+str(i), "r") as f:
                 content = f.readlines()
-                T1[i] = map(float, content)
+                T1[i] = list(map(float, content))
 
-    plt.plot(vals, list(T2), label="1 process", drawstyle="steps")
+    plt.plot(vals, T2, label="1 process", drawstyle="steps", alpha=0.3)
     for key in T1:
-        plt.plot(vals, list(T1[key]), label="{} processes".format(key), drawstyle="steps")
+        plt.plot(vals, T1[key], label="{} processes".format(key), drawstyle="steps", alpha=0.3)
     
+    vals = list(T1.values())
+    T2_x, T2_y = denoise(T2, resolution=500) 
+    T1 = [denoise(vals[i], resolution=1000) for i in range(len(vals))]
+    plt.plot(T2_x, T2_y, label="interpolated 1 process")
+    for i in range(len(T1)):
+        plt.plot(T1[i][0], T1[i][1], label="interpolated {} processes".format(procs[i]))
+
     plt.title("n!")
     plt.ylabel("time/s")
     plt.xlabel("n")
