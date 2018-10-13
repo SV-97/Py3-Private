8c20c2efb514c39241d9aaabeb32346d263f8495
SV-97 <volzstefan97@googlemail.com>
2018-10-13 19:52:09 +0200

    Renamed some variables

--- P3_036_BarcodeReader/P3_036_BarcodeReader.py
+++ P3_036_BarcodeReader/P3_036_BarcodeReader.py
@@ -19,14 +19,14 @@ for path in paths:
         x, y, w, h = barcode.rect
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
     
-        barcodeData = barcode.data.decode("utf-8")
-        barcodeType = barcode.type
+        barcode_data = barcode.data.decode("utf-8")
+        barcode_type = barcode.type
     
-        text = "{} ({})".format(barcodeData, barcodeType)
+        text = "{} ({})".format(barcode_data, barcode_type)
         cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
             0.5, (0, 0, 255), 2)
     
-        print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+        print("[INFO] Found {} barcode: {}".format(barcode_type, barcode_data))
 
     name = "Barcode{}".format(paths.index(path))
     cv2.namedWindow(name)
